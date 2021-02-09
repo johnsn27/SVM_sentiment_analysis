3bc89be4a513632dd487d2242de12a49d13f7e90
johnsn27 <nathan.johnson@bbc.co.uk>
2021-02-09 18:00:32 +0000

    fix run

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+from threading import Thread
+
 from flask import Flask, jsonify, make_response, request
 from app.fast_response import fast_response
 from app.gather_data.new_articles import read_csv
+from app.svm_linear import train_model
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -10,17 +13,17 @@ app.config['JSON_AS_ASCII'] = False
 def sentiment_analysis():
     """"method that outputs a response"""
     if request.method == 'GET':
-        # retrain_model = Thread(target=train_model)
-        # retrain_model.start()
+        retrain_model = Thread(target=train_model)
+        retrain_model.start()
         text = 'happy to the point of sadness'
         return fast_response(text)
     return make_response(jsonify({'error': 'sorry! unable to parse', 'status_code': 500}), 500)
 
 
-# @app.route('/gather_data', methods=['GET', 'POST'])
-# def gather_data():
-#     """"method that outputs a response"""
-#     read_csv()
+@app.route('/gather_data', methods=['GET', 'POST'])
+def gather_data():
+    """"method that outputs a response"""
+    read_csv()
 
 
 if __name__ == '__main__':
