d111baaea677df4e709ba6bebdf62d60257ebb70
johnsn27 <nathan.johnson@bbc.co.uk>
2021-02-15 15:46:32 +0000

    linting

--- app/random_forest/rf_create_model.py
+++ app/random_forest/rf_create_model.py
@@ -1,11 +1,11 @@
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
 import pickle
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.feature_extraction.text import TfidfVectorizer
-import os
 
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
@@ -16,17 +16,20 @@ imdb = pd.read_csv('kaggle/input/Train.csv')
 features = imdb.drop("label", axis=1)
 labels = imdb["label"]
 
-X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.90, random_state=42)
-X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.5, random_state=42)
-X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
+X_train, X_test, y_train, y_test = \
+    train_test_split(features, labels, test_size=0.90, random_state=42)
+X_train, X_test, y_train, y_test = \
+    train_test_split(X_train, y_train, test_size=0.5, random_state=42)
+X_val, X_test, y_val, y_test = \
+    train_test_split(X_test, y_test, test_size=0.5, random_state=42)
 
 
 def vectorize(data, tfidf_vect_fit):
-    X_tfidf = tfidf_vect_fit.transform(data)
+    x_tfidf = tfidf_vect_fit.transform(data)
     words = tfidf_vect_fit.get_feature_names()
-    X_tfidf_df = pd.DataFrame(X_tfidf.toarray())
-    X_tfidf_df.columns = words
-    return (X_tfidf_df)
+    x_tfidf_df = pd.DataFrame(x_tfidf.toarray())
+    x_tfidf_df.columns = words
+    return x_tfidf_df
 
 
 tfidf_vectorizer = TfidfVectorizer()
