a445bb6589aa449e73ec881d386dc5f0e63e9cd3
johnsn27 <nathan.johnson@bbc.co.uk>
2021-02-15 20:36:22 +0000

    Refactor

--- app/random_forest/rf_create_model.py
+++ app/random_forest/rf_create_model.py
@@ -3,22 +3,7 @@ import pandas as pd
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-
-# todo: refactor this so that its functional programming styled
-dataset = pd.read_csv('../data/trainRF.csv')
-
-features = dataset.drop("label", axis=1)
-labels = dataset["label"]
-
-a_train, a_test, b_train, b_test = \
-    train_test_split(features, labels, test_size=0.90, random_state=42)
-a_train, a_test, b_train, b_test = \
-    train_test_split(a_train, b_train, test_size=0.5, random_state=42)
-a_val, a_test, b_val, b_test = \
-    train_test_split(a_test, b_test, test_size=0.5, random_state=42)
 
 
 def vectorise(data, tfidf_vectorizer_fitted):
@@ -30,26 +15,32 @@ def vectorise(data, tfidf_vectorizer_fitted):
     return tfidf_df
 
 
-tfidf_vectorizer = TfidfVectorizer(min_df=5,
-                                   max_df=0.8,
-                                   strip_accents='ascii',
-                                   lowercase=True,
-                                   use_idf=True)
+def create_model():
+    dataset = pd.read_csv('../data/trainRF.csv')
 
-tfidf_vectorizer_fit = tfidf_vectorizer.fit(a_train['text'])
-a_train = vectorise(a_train['text'], tfidf_vectorizer_fit)
+    sentiment = "label"
+    text = 'text'
 
-a_val = vectorise(a_val['text'], tfidf_vectorizer_fit)
+    features = dataset.drop(sentiment, axis=1)
+    labels = dataset[sentiment]
 
-rf_classifier = RandomForestClassifier(n_estimators=100, max_depth=None)
-rf_classifier.fit(a_train, b_train.values.ravel())
 
-b_pred = rf_classifier.predict(a_val)
-accuracy = round(accuracy_score(b_val, b_pred), 3)
-precision = round(precision_score(b_val, b_pred), 3)
-recall = round(recall_score(b_val, b_pred), 3)
+    a_train, a_test, b_train, b_test = \
+        train_test_split(features, labels, test_size=0.90, random_state=42)
+    a_train, a_test, b_train, b_test = \
+        train_test_split(a_train, b_train, test_size=0.5, random_state=42)
 
-a_test = vectorise(a_test['text'], tfidf_vectorizer_fit)
+    tfidf_vectorizer = TfidfVectorizer(min_df=5,
+                                       max_df=0.8,
+                                       strip_accents='ascii',
+                                       lowercase=True,
+                                       use_idf=True)
 
-pickle.dump(a_train, open('../models/train_data_RF.sav', 'wb'))
-pickle.dump(rf_classifier, open('../models/classifierRF.sav', 'wb'))
+    tfidf_vectorizer_fit = tfidf_vectorizer.fit(a_train[text])
+    a_train = vectorise(a_train[text], tfidf_vectorizer_fit)
+
+    rf_classifier = RandomForestClassifier(n_estimators=100, max_depth=None)
+    rf_classifier.fit(a_train, b_train.values.ravel())
+
+    pickle.dump(a_train, open('../models/train_data_RF.sav', 'wb'))
+    pickle.dump(rf_classifier, open('../models/classifierRF.sav', 'wb'))
