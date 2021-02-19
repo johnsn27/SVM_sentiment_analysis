a2a0b482b7a9529a50cf0c166cd38fd49189cdbc
Nathan Johnson <nathan.johnson@bbc.co.uk>
2021-02-19 18:08:12 +0000

    Create highlight_words function (#29)
    * Create highlight_words function
    
    * Add docstring

--- app/random_forest/rf_visualisation.py
+++ app/random_forest/rf_visualisation.py
@@ -3,15 +3,33 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def rf_create_graph():
-    """"Creates a bar chart of the most important features """
+def get_list_of_most_extreme_words(num):
+    """returns a list of the most extreme words"""
     train_data = pickle.load(open('../models/train_data_RF.sav', 'rb'))
     classifier = pickle.load(open('../models/classifierRF.sav', 'rb'))
-    feat_importances = pd.Series(classifier.feature_importances_, index=train_data.columns)
-    feat_importances.nlargest(20).plot(kind='bar', figsize=(10, 10))
+    list_of_words = pd.Series(classifier.feature_importances_, index=train_data.columns)
+    most_extreme_words = list_of_words.nlargest(num)
+    return most_extreme_words.keys()
+
+
+def rf_create_graph():
+    """"Creates a bar chart of the most important features """
+    get_list_of_most_extreme_words(20).plot(kind='bar', figsize=(10, 10))
     plt.title("Top 20 important features")
     plt.show()
 
 
+def highlight_words(text):
+    """returns the words in the text that caused the sentiment to be too extreme"""
+    most_extreme_words = get_list_of_most_extreme_words(61)
+    list_of_input_words = text.split(" ")
+    words_to_highlight =[]
+    for word in most_extreme_words:
+        for inputted_word in list_of_input_words:
+            if word == inputted_word:
+                words_to_highlight.append(word)
+    return words_to_highlight
+
+
 if __name__ == '__main__':
-    rf_create_graph()
+    highlight_words("This was the worst, just awful")
